259d1ec8f46e2250163923957655c2444cab8b99
Milena Gasiorek <milena.gasiorek1627@gmail.com>
2020-04-19 17:53:16 +0200

    main navigation

---
+++ js/main-navigation.js
@@ -0,0 +1,14 @@
+(() => {
+    const button = document.querySelector(".js-main-navigation_mobile-button");
+    const menu = document.querySelector(".js-main-navigation_menu")
+
+    const toggleClass = () =>{
+
+
+        menu.classList.toggle("main-navigation_menu--open");
+    };
+
+    button.addEventListener("click", toggleClass);
+    menu.addEventListener("click", toggleClass);
+
+})();
